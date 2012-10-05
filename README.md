9c272d9f9e48f781b4b395b9bb0976c976003cf8
Dennis Roberts <dennis@iplantcollaborative.org>
2012-10-04 18:47:35 -0700

    CORE-3539: initial attempt at packaging the DE inan RPM; some details still need to be worked out

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# de-jetty
+
+A project to allow the iPlant Discovery Environment web application to be
+packaged as an RPM for deployment to a Jetty server that is launched using
+jetty-runner.
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject org.iplantc/iplant-de "1.4.2-SNAPSHOT"
+  :description  "Project for generating discovery environment RPMs."
+  :dependencies  [[org.iplantc.de/de-web "1.4.2-SNAPSHOT" :extension "war"]
+                  [org.mortbay.jetty/jetty-runner "8.1.7.v20120910"]]
+  :plugins      [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
+  :iplant-rpm   {:summary      "iPlant Discovery Environment"
+                 :provides     "iplant-de"
+                 :dependencies ["iplant-service-config >= 0.1.0-5"]
+                 :config-files ["log4j.properties"]
+                 :config-path  "conf"
+                 :type         :jetty}
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
