037169989983dcb9322ef4f4a768cc767128abbd
Dennis Roberts <dennis@iplantcollaborative.org>
2013-04-08 08:23:41 -0700

    switched to the GXT3 branch of the DE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.iplantc/iplant-de "1.6-SNAPSHOT"
+(defproject org.iplantc/iplant-de "1.8-SNAPSHOT"
   :description  "Project for generating discovery environment RPMs."
-  :dependencies  [[org.iplantc.de/de-web "dev-SNAPSHOT" :extension "war"]
+  :dependencies  [[org.iplantc.de/de-web "gxt3Sandbox-SNAPSHOT" :extension "war"]
                   [org.mortbay.jetty/jetty-runner "8.1.7.v20120910"]]
-  :plugins      [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
+  :plugins      [[org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
   :iplant-rpm   {:summary      "iPlant Discovery Environment"
                  :provides     "iplant-de"
                  :dependencies ["iplant-service-config >= 0.1.0-5"]
